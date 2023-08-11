3445e00f8792ebe18217fe727f1bc3ca7f5d41be
BelieveMb <124797985+BelieveMb@users.noreply.github.com>
2023-08-11 07:50:53 +0000

    write hello

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
 // Do not remove this function definition and its export
 // this function should return the result of the current operation
+//hello
 function calculate(expression) {
   return expression;
 }
