a0d405fa85a0070e1d2fc421743ba07f66a2521b
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-08-14 11:55:11 +0200

    Recuperate and Add event  to buttons

--- script.js
+++ script.js
@@ -5,61 +5,32 @@ import { calculate } from './calculator.js';
 const showResult = document.getElementById('calcul');
 const bigInput = document.getElementById('input');
 //this var exist in the first part of calculator, for show the result
+//our functions
+const btn = document.querySelectorAll('.digit');
 
-//place of buttons 
+//place of buttons
 const reset = document.getElementById('reset');
 const clear = document.getElementById('clear');
 const plusoumoins = document.getElementById('plusoumoins');
 const percentage = document.getElementById('percentage');
 const divideby = document.getElementById('divideby');
-const btn7 = document.querySelectorAll('.digit')[0];
-const btn8 = document.querySelectorAll('.digit')[1];
-const btn9 = document.querySelectorAll('.digit')[2];
-const btnX = document.getElementById('times');
-const btn4 = document.querySelectorAll('.digit')[3];
-const btn5 = document.querySelectorAll('.digit')[4];
-const btn6 = document.querySelectorAll('.digit')[5];
 const btnMoins = document.getElementById('minus');
-const btn1 = document.querySelectorAll('.digit')[6];
-const btn2 = document.querySelectorAll('.digit')[7];
-const btn3 = document.querySelectorAll('.digit')[8];
 const btnPlus = document.getElementById('plus');
-const point = document.querySelector('.dot');
-const btn0 = document.querySelectorAll('.digit')[9];
 const egal = document.getElementById('equals');
+const point = document.querySelector('.dot');
+const btnX = document.getElementById('times');
+
 //reset variable
 showResult.innerHTML ="";
 bigInput.disabled = true;
-// Add event  to buttons
-btn0.addEventListener("click", function(){
-    bigInput.value += 0;
-});
-btn1.addEventListener("click", function(){
-    bigInput.value += 1;
-});
-btn2.addEventListener("click", function(){
-    bigInput.value += 2;
-});
-btn3.addEventListener("click", function(){
-    bigInput.value += 3;
-});
-btn4.addEventListener("click", function(){
-    bigInput.value += 4;
-});
-btn5.addEventListener("click", function(){
-    bigInput.value += 5;
-});
-btn6.addEventListener("click", function(){
-    bigInput.value += 6;
-});
-btn7.addEventListener("click", function(){
-    bigInput.value += 7;
-});
-btn8.addEventListener("click", function(){
-    bigInput.value += 8;
-});
-btn9.addEventListener("click", function(){
-    bigInput.value += 9;
+// Recuperate and Add event  to buttons
+btn.forEach(btnIndex => {
+    btnIndex.addEventListener('click', function(){
+        let btnContent = btnIndex.textContent;
+        bigInput.value += btnContent;
+        console.log(btnIndex.textContent);
+
+    });
 });
 point.addEventListener("click", function(){
     bigInput.value += '.';
@@ -74,15 +45,19 @@ clear.addEventListener("click", function(){
 });
 //add +/- on double click
 plusoumoins.addEventListener("click", function(){
-    bigInput.value = "-" + bigInput.value; 
+    let opposite = parseInt(bigInput.value) * -1;
+    bigInput.value =  opposite; 
 });
+// plusoumoins.addEventListener("dblclick", function (){
+//     bigInput.value = "5" + bigInput.value; 
+// });
 //change the submit of button
-percentage.setAttribute("type", "button");
-divideby.setAttribute("type", "button");
-btnX.setAttribute("type", "button");
-btnMoins.setAttribute("type", "button");
-btnPlus.setAttribute("type", "button");
-egal.setAttribute("type", "button");
+// percentage.setAttribute("type", "button");
+// divideby.setAttribute("type", "button");
+// btnX.setAttribute("type", "button");
+// btnMoins.setAttribute("type", "button");
+// btnPlus.setAttribute("type", "button");
+// egal.setAttribute("type", "button");
 //percentage
 //best operator, if on click in operator, we change de showresult
 percentage.addEventListener("click", function(){
