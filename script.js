3307b92af64729c8bb983e195a2204d9ae1af8bb
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-08-16 12:26:58 +0200

    update the function addPoint

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ const bigInput = document.getElementById('input');
 //this var exist in the first part of calculator, for show the result
 const btn = document.querySelectorAll('.digit');
 // ours functions
-function addPoint(){
-    bigInput.value += '.';                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-    point.removeEventListener("click", addPoint);
+function addPoint() {
+    if (bigInput.value === null || typeof bigInput.value === 'undefined'){
+        bigInput.value += '.';
+        point.removeEventListener("click", addPoint);
+    }else{
+        bigInput.value = '';
+    }
 }
 //place of buttons
 const reset = document.getElementById('reset');
@@ -23,79 +27,76 @@ const egal = document.getElementById('equals');
 const point = document.querySelector('.dot');
 const btnX = document.getElementById('times');
 //select many operators
-// const btnOpera = document.querySelectorAll('#divideby, #minus, #plus, #times');
-// console.log(btnOpera);
-// btnOpera.forEach(btnOp => {
-//     btnOp.addEventListener('click', function(){
-//         let btnContent = btnOp.textContent;
-//         bigInput.value += btnContent;
-//     });
-// });
 //reset variable
-showResult.innerHTML ="";
+showResult.innerHTML = "";
 bigInput.disabled = true;
 // Recuperate and Add event  to buttons
 btn.forEach(btnIndex => {
-    btnIndex.addEventListener('click', function(){
+    btnIndex.addEventListener('click', function () {
         let btnContent = btnIndex.textContent;
-        bigInput.value += btnContent;
+        if (typeof(bigInput.value) !== "undefined") {
+            bigInput.value += btnContent;
+        }
+
     });
 });
 
 point.addEventListener("click", addPoint);
 // Add event to operators
-reset.addEventListener("click", function(){
+reset.addEventListener("click", function () {
     showResult.innerHTML = "";
-    bigInput.value ="";
+    bigInput.value = "";
 });
-clear.addEventListener("click", function(){
-    bigInput.value ="";
+clear.addEventListener("click", function () {
+    bigInput.value = "";
 });
 //add +/- on double click
-plusoumoins.addEventListener("click", function(){
+plusoumoins.addEventListener("click", function () {
     let opposite = parseInt(bigInput.value) * -1;
-    bigInput.value =  opposite; 
+    bigInput.value = opposite;
 });
 //percentage, if on click in operator, we change de showresult
-percentage.addEventListener("click", function(event){
+percentage.addEventListener("click", function (event) {
     let calPercentage;
     calPercentage = bigInput.value / 100;
     showResult.innerHTML = bigInput.value + "/100";
     bigInput.value = calPercentage;
     event.preventDefault();
 });
-divideby.addEventListener("click", function(event){
+divideby.addEventListener("click", function (event) {
     showResult.innerHTML = bigInput.value + "%";
     bigInput.value = "";
     event.preventDefault();
 });
-btnX.addEventListener("click", function(event){
+btnX.addEventListener("click", function (event) {
     showResult.innerHTML = bigInput.value + "*";
     bigInput.value = "";
     event.preventDefault();
 });
-btnMoins.addEventListener("click", function(event){
+btnMoins.addEventListener("click", function (event) {
     showResult.innerHTML = bigInput.value + "-";
     bigInput.value = "";
     event.preventDefault();
 });
-btnPlus.addEventListener("click", function(event){
+btnPlus.addEventListener("click", function (event) {
     showResult.innerHTML = bigInput.value + "+";
     bigInput.value = "";
     event.preventDefault();
 });
-function btnEgal(event){
+
+function btnEgal(event) {
     event.preventDefault();
-    if(bigInput.value){
-        if(bigInput.value !== "Error, It's empty"){
+    // egal.removeEventListener("click", btnEgal);    
+    if (bigInput.value) {
+        if (bigInput.value !== "Error, It's empty") {
             let Docalcul = showResult.innerHTML += bigInput.value;
             let reponse = eval(Docalcul);
             bigInput.value = reponse;
         }
-    }else{
+    } else {
         bigInput.value = "Error, It's empty";
     }
-    egal.removeEventListener("click", btnEgal);    
+
 }
 //last part egal
 egal.addEventListener("click", btnEgal);
